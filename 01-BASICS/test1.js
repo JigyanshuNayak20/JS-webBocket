92a796ac766d7d4761c33b0c3406b6d8d35bf230
JigyanshuNayak20 <152247017+JigyanshuNayak20@users.noreply.github.com>
2023-11-29 04:22:15 +0000

    File Added

---
+++ 01-BASICS/test1.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+/*
+1.JavaScript is a High level scripting proggraming language used on devloping web & moile aplication efficiently.
+2.We use javascript in both client side as well as server side language.
+3.We use javasript for devloping logics in our code.
+4.Javascript was devloped in 1997.
+5.Now these days we used the Es6 version of JS it was started on 2015 and now it is the latest version of us.
+/*

c16a3565ff4975f6058a391b1c182af171d80530
JigyanshuNayak20 <152247017+JigyanshuNayak20@users.noreply.github.com>
2023-11-30 04:20:07 +0000

    file added

---
+++ 01-BASICS/Datatypes.js
@@ -0,0 +1,16 @@
+
+let name="jigyanshu";
+let age=24;
+let ispresent=true;
+let salary=6000000000877864667558765557664457645n;
+let city=null;
+let bhubaneswar;
+let id=Symbol('123');
+let anotherid=Symbol('123');
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof ispresent);
+console.log(typeof salary);
+console.log(typeof city);
+console.log(typeof bhubaneswar);
+console.log(id===anotherid);

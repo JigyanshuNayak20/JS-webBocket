387d37ab203bb137defc12c944dfd5d342554d14
JigyanshuNayak20 <152247017+JigyanshuNayak20@users.noreply.github.com>
2023-11-30 05:03:34 +0000

    File Added

---
+++ 01-BASICS/Operation.js
@@ -0,0 +1,22 @@
+let value=3
+let negvalue=-value;
+let str1="gift";
+let str2="gita";
+let str3=str1+str2;
+console.log(str3);
+console.log(negvalue);//-3
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log("1"+2+"2");
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter);
+gamecounter++;
+console.log(gamecounter);
